37e84713e14cbe27e34115e3b8f621956c5f1cf9
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-06 00:31:17 +0800

    242. adding state slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,31 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
-const defaultState = {
+const initialState = {
   counter: 0,
   showCounter: true,
 };
 
-const counterReducer = (state = defaultState, action) => {
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
