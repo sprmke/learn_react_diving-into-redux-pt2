e85c529d743b93797386efc416c711d309e8898e
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-06 01:58:46 +0800

    245. working with multiple states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from './components/Header';
+import Auth from './components/Auth';
 import Counter from './components/Counter';
-
 
 function App() {
   return (
-    <Counter />
+    <>
+      <Header />
+      <Auth />
+      <Counter />
+    </>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -24,13 +24,31 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    // other: counterSlice.other,
+    auth: authSlice.reducer,
   },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
