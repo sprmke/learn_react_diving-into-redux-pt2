61252e46f62b751edd7a73c3d0e46fddccdec627
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-06 02:09:36 +0800

    246. reading & dispatching from a new slice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useSelector } from 'react-redux';
+
 import Header from './components/Header';
 import Auth from './components/Auth';
 import Counter from './components/Counter';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </>
   );
